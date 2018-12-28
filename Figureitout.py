767b9df0cbfe512ff6147ed7ca4f9e3fd6974516
jpiercemelfigureitout <jpiercemel@verizon.net>
2018-12-28 11:56:44 -0500

    Update Figureitout.py

--- Figureitout.py
+++ Figureitout.py
@@ -6,14 +6,17 @@ def figure_it_out():
     wordy = str(raw_input ('Enter a word to guess letters:    '))
     guess = str(raw_input ('Guess the letters in the secrect word.'))
     score = 0
+    wrong = 0
     for letter in wordy:
         if letter in guess:
             score += 1
             print (letter, end="") 
             
         else:
+            wrong +=1
             if letter == " ":
                 print(" ",end="")
             else:
                 print ("-",end="")
-    print ('Your score is ', score, ' !')
+    print ('Your score is ', score, '!')
+    print ('The number of wrong letters is ',wrong, '.') 
